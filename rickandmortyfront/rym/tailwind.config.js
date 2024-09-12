e45a230df2deaaa7dfb66c195916951196df379e
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-09-12 10:35:09 +0200

    responsive header

--- rickandmortyfront/rym/tailwind.config.js
+++ rickandmortyfront/rym/tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* basic colors */
+        'background-color': '#161814ff',
+        'dark-color-1': '#2b2d29ff',
+        'dark-color-2': '#424440ff',
+        'dark-color-3': '#5a5b58ff',
+        'dark-color-4': '#737472ff',
+        'light-color': '#8d8f8cff',
+
+        /* palette color */
+        'light-green-bright': '#c0f661ff',
+        'light-green-pastel': '#c6f96bff',
+        'dark-green-forest': '#304d1bff',
+        'medium-green': '#68ac52ff',
+        'light-green-moss': '#7bbb6bff',
+        'dark-green-leaf': '#355728ff',
+      },
+    }
   },
   plugins: [
     require('flowbite/plugin'),
