8398b04c2dcb491ca77a5ebcdff54a7ce9b4ce20
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-09-12 14:30:45 +0200

    cards style

--- rickandmortyfront/rym/tailwind.config.js
+++ rickandmortyfront/rym/tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         /* basic colors */
         'background-color': '#161814ff',
         'dark-color-1': '#2b2d29ff',
+        'dark-color-1-5': '#363935ff',
         'dark-color-2': '#424440ff',
         'dark-color-3': '#5a5b58ff',
         'dark-color-4': '#737472ff',
