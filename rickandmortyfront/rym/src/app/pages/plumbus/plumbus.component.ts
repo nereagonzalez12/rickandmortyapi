105f8bf37b772f716dc49e1ada0732d5017e572e
nereagonzalez12 <ereadoce.ng@gmail.com>
2024-09-25 13:01:01 +0200

    idiom bug fixed

--- rickandmortyfront/rym/src/app/pages/plumbus/plumbus.component.ts
+++ rickandmortyfront/rym/src/app/pages/plumbus/plumbus.component.ts
@@ -1,23 +1,42 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-plumbus',
   templateUrl: './plumbus.component.html',
   styleUrls: ['./plumbus.component.css']
 })
-export class PlumbusComponent {
-
+export class PlumbusComponent implements OnInit {
+  selectedLang: string | null = '';
   navHidden: boolean = true;
 
   constructor(public translate: TranslateService) {
-    translate.addLangs(['en', 'es']);
-    translate.setDefaultLang('en');
 
-    const browserLang = translate.getBrowserLang();
-    if (browserLang) {
-      translate.use(browserLang.match(/en|es/) ? browserLang : 'en');
+    translate.addLangs(['en', 'es']);
+    this.selectedLang = sessionStorage.getItem('lang');
+
+    if (this.selectedLang) {
+      translate.setDefaultLang(this.selectedLang);
+      translate.use(this.selectedLang);
+    } else {
+      const browserLang = translate.getBrowserLang();
+      if (browserLang) {
+        const langToUse = browserLang.match(/en|es/) ? browserLang : 'en';
+        translate.setDefaultLang(langToUse);
+        translate.use(langToUse);
+      }
     }
+  }
+
+
+  ngOnInit(): void {
+
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      if (event) {
+        sessionStorage.setItem('lang', event.lang);
+      }
+    });
+
   }
 
   showNav() {
